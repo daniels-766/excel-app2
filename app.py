05393030d729a5ca4d063f3f3c931f628b394d27
daniel <daniel@uatas.id>
2024-10-16 11:45:25 +0700

    change db password

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b35dfe6ce150230940bd145823034485'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/app_excel3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Vjr@1234567890@localhost/app_excel3'
 app.config['MAX_CONTENT_LENGTH'] = 150 * 1024 * 1024  # 150 MB
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['SESSION_TYPE'] = 'filesystem'  # Atau 'redis', 'memcached', dll.
