5afb22ed79732cb5c1b8f586a2a5f9849b9989be
daniel <daniel@uatas.id>
2024-10-18 16:07:14 +0700

    add admin acc

--- create_users.py
+++ create_users.py
@@ -3,7 +3,7 @@ from app import app, db, User, bcrypt
 # Buat konteks aplikasi
 with app.app_context():
     # Create admin user
-    admin = User(username='admin', password_hash=bcrypt.generate_password_hash('12345678').decode('utf-8'), role='admin')
+    admin = User(username='Willy J Leasiwal', password_hash=bcrypt.generate_password_hash('willy@888').decode('utf-8'), role='admin')
     db.session.add(admin)
 
     # Commit changes to the database
